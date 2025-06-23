c5cf488a34bd6a5b6b84651469e3806be3a459a0
MD. Sifat Islam <sifat706401@gmail.com>
2025-06-23 20:22:20 +0600

    Initial Commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    {/* <App /> */}
+    <h1>runnign..</h1>
+  </StrictMode>,
+)
